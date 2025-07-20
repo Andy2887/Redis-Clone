fdcef5eb6cdb7a0d2e1295fbd7d9f66a0b514f97
Andy2887 <yuanliheng05@gmail.com>
2025-07-20 19:27:55 -0400

    implemented respond to PING

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,10 @@
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
   public static void main(String[] args){
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
-
        ServerSocket serverSocket = null;
        Socket clientSocket = null;
        int port = 6379;
@@ -17,6 +15,8 @@ public class Main {
          serverSocket.setReuseAddress(true);
          // Wait for connection from client.
          clientSocket = serverSocket.accept();
+         OutputStream outputStream = clientSocket.getOutputStream();
+         outputStream.write("+PONG\r\n".getBytes());
        } catch (IOException e) {
          System.out.println("IOException: " + e.getMessage());
        } finally {
