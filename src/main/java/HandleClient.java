3ddf0193f49ba3c0e0428dc9cd136597cddc495c
Andy2887 <yuanliheng05@gmail.com>
2025-07-29 20:02:50 -0400

    removed unused helper function

--- src/main/java/HandleClient.java
+++ src/main/java/HandleClient.java
@@ -1033,17 +1033,6 @@ public class HandleClient implements Runnable {
     }
   }
 
-  // Helper to convert hex string to byte array
-  private static byte[] hexStringToByteArray(String s) {
-      int len = s.length();
-      byte[] data = new byte[len / 2];
-      for (int i = 0; i < len; i += 2) {
-          data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
-                               + Character.digit(s.charAt(i+1), 16));
-      }
-      return data;
-  }
-
   // Propagate write command to replica if connected
   private void propagateToReplica(List<String> command) {
     synchronized (HandleClient.class) {
